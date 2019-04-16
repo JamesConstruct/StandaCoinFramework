dbd1b24cf9ef18b1d678cce868941585e2cb4935
JamesConstruct <33464236+JamesConstruct@users.noreply.github.com>
2019-04-16 17:41:41 +0200

    Update StandaCoin.php

--- src/StandaCoin.php
+++ src/StandaCoin.php
@@ -43,12 +43,16 @@ class App {
 
     private function __random_string($len=5)
     {
+        
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $randstring = '';
+        
         for ($i = 0; $i < $len; $i++) {
             $randstring .= $characters[rand(0, strlen($characters)-1)];
         }
+        
         return $randstring;
+        
     }
     
 
